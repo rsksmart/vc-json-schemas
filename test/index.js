64b62258fbbe731f133d84ae0ceb05c86281f71a
Ilan Olkies <ilanolkies@outlook.com>
2020-11-20 05:01:33 -0300

    Clean up

--- test/index.js
+++ test/index.js
@@ -2,15 +2,13 @@ const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 const Ajv = require('ajv').default
-const { Resolver } = require('did-resolver')
-const EthrDIDResolver = require('ethr-did-resolver')
 const { verifyJWT } = require('did-jwt')
+const resolver = require('../util/didResolver')
 
 const ajv = new Ajv()
 
 // Assert Schemas are Base Schema complaint
 const vcJSONSchema = require('../base-schema')
-const { parse } = require('path')
 
 const validateBaseSchema = ajv.compile(vcJSONSchema)
 
@@ -23,13 +21,5 @@ const validateEmailSchema = ajv.compile(emailCredentialSchema.schema)
 
 const sampleCredentialJWT = fs.readFileSync(path.resolve(__dirname, '../sample/EmailCredentialSchema/v1.0/sample-1')).toString()
 
-const ethrDIDResolver = EthrDIDResolver.getResolver({
-  networks: [
-    { name: 'rsk', rpcUrl: 'https://did.rsk.co:4444' }
-  ]
-})
-
-let resolver = new Resolver(ethrDIDResolver)
-
-verifyJWT(sampleCredentialJWT, { resolver } )
+verifyJWT(sampleCredentialJWT, { resolver })
   .then(({ payload }) => validateEmailSchema(payload.credentialSubject))
