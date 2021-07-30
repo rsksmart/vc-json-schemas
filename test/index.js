1e4fa96386ff07a85126684cc4a1602619db0a5d
Ilan <ilanolkies@outlook.com>
2021-07-30 16:31:29 -0300

    Add Phone VC Schema

--- test/index.js
+++ test/index.js
@@ -12,14 +12,35 @@ const vcJSONSchema = require('../base-schema')
 
 const validateBaseSchema = ajv.compile(vcJSONSchema)
 
-const emailCredentialSchema = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../schema/EmailCredentialSchema/v1.0/schema.json')))
+const fixture = {
+  'Email': {
+    schema: '../schema/EmailCredentialSchema/v1.0/schema.json',
+    sample: '../sample/EmailCredentialSchema/v1.0/sample-1'
+  },
+  'Phone': {
+    schema: '../schema/PhoneCredentialSchema/v1.0/schema.json',
+    sample: '../sample/PhoneCredentialSchema/v1.0/sample-1'
+  }
+}
 
-assert(validateBaseSchema(emailCredentialSchema), 'Not compliant with VC JSON Schemas v1.0')
+const test = async () => {
+  for (let schemaName of Object.keys(fixture)) {
+    const { schema, sample } = fixture[schemaName]
 
-// Assert Samples are Schema complaint
-const validateEmailSchema = ajv.compile(emailCredentialSchema.schema)
+    const emailCredentialSchema = JSON.parse(fs.readFileSync(path.resolve(__dirname, schema)))
+    assert(validateBaseSchema(emailCredentialSchema), `${schemaName} Credential Schema not compliant with VC JSON Schemas v1.0`)
 
-const sampleCredentialJWT = fs.readFileSync(path.resolve(__dirname, '../sample/EmailCredentialSchema/v1.0/sample-1')).toString()
+    const validateSchema = ajv.compile(emailCredentialSchema.schema)
+    const sampleCredentialJWT = fs.readFileSync(path.resolve(__dirname, sample)).toString()
+    await verifyJWT(sampleCredentialJWT, { resolver })
+      .then(({ payload }) => validateSchema(payload.credentialSubject))
+  }
+}
 
-verifyJWT(sampleCredentialJWT, { resolver })
-  .then(({ payload }) => validateEmailSchema(payload.credentialSubject))
+test().then(() => {
+  console.log('Success!')
+  process.exit(0)
+}).catch(e => {
+  console.log('Failed with error', e)
+  process.exit(1)
+})
